f6bd4285c457451c42546b7d85bdd524758d15a4
philip ka sing leung <philip.leung90@gmail.com>
2017-12-09 21:59:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function theBeatlesPlay(arrayM, arrayI){
   var newArray = [];
-  for(let )
+  for(let i = 0 ; i< arrayM.length; i++){
+    newArray.push(arrayM[0] + arrayI[0]);
+  }
+  
+  return newArray;
+  
 }
